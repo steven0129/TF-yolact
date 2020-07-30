a25206a017a2c9908f39c2dc9381501efdd3a55a
steven.gao <steven.gao@realtek.com>
2020-07-30 16:13:07 +0800

    Train with 13 classes.
    Signed-off-by: steven.gao <steven.gao@realtek.com>

--- train.py
+++ train.py
@@ -123,7 +123,7 @@ def main(argv):
     YOLACT = lite.MyYolact(input_size=256,
                           fpn_channels=160,
                           feature_map_size=[69, 35, 18, 9, 5],
-                          num_class=12, # 11 classes + 1 background
+                          num_class=13, # 12 classes + 1 background
                           num_mask=32,
                           aspect_ratio=[1, 0.5, 2],
                           scales=[24, 48, 96, 192, 384])
