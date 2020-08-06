8a4d971c34de52619c6f199b7f0d5f83a99c603a
steven.gao <steven.gao@realtek.com>
2020-08-06 14:03:43 +0800

    Data augmentation with horizontal flip
    Signed-off-by: steven.gao <steven.gao@realtek.com>

--- data/create_coco_tfrecord.py
+++ data/create_coco_tfrecord.py
@@ -85,6 +85,243 @@ def GenDPMask(ann):
 
     return mask_face, mask_body
 
+def create_tf_horizontal_flip_example(image,
+                      annotations_list,
+                      image_dir,
+                      category_index):
+    """Converts image and annotations to a tf.Example proto.
+
+    Args:
+      image: dict with keys:
+        [u'license', u'file_name', u'coco_url', u'height', u'width',
+        u'date_captured', u'flickr_url', u'id']
+      annotations_list:
+        list of dicts with keys:
+        [u'segmentation', u'area', u'iscrowd', u'image_id',
+        u'bbox', u'category_id', u'id']
+        Notice that bounding box coordinates in the official COCO dataset are
+        given as [x, y, width, height] tuples using absolute coordinates where
+        x, y represent the top-left (0-indexed) corner.  This function converts
+        to the format expected by the Tensorflow Object Detection API (which is
+        which is [ymin, xmin, ymax, xmax] with coordinates normalized relative
+        to image size).
+      image_dir: directory containing the image files.
+      category_index: a dict containing COCO category information keyed
+        by the 'id' field of each category.  See the
+        label_map_util.create_category_index function.
+    Returns:
+      example: The converted tf.Example
+      num_annotations_skipped: Number of (invalid) annotations that were ignored.
+
+    Raises:
+      ValueError: if the image pointed to by data['filename'] is not a valid JPEG
+    """
+    image_height = image['height']
+    image_width = image['width']
+    filename = image['file_name']
+    image_id = image['id']
+
+    full_path = os.path.join(image_dir, filename)
+    with tf.io.gfile.GFile(full_path, 'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = PIL.Image.open(encoded_jpg_io)
+    r = 320
+    image = image.resize((r, r), PIL.Image.ANTIALIAS)
+    image = image.transpose(PIL.Image.FLIP_LEFT_RIGHT)   # Image Horizontal Flip
+    bytes_io = io.BytesIO()
+    image.save(bytes_io, format='JPEG')
+    encoded_jpg = bytes_io.getvalue()
+    key = hashlib.sha256(encoded_jpg).hexdigest()
+
+    xmin = []
+    xmax = []
+    ymin = []
+    ymax = []
+    is_crowd = []
+    category_names = []
+    category_ids = []
+    area = []
+    encoded_mask_png = []
+    num_annotations_skipped = 0
+    dp_triggered = True  # DensePose preprocessing just one time
+
+    for object_annotations in annotations_list:
+        category_id = int(object_annotations['category_id'])
+
+        (x, y, width, height) = tuple(object_annotations['bbox'])
+        if width <= 0 or height <= 0:
+            num_annotations_skipped += 1
+            continue
+        if x + width > image_width or y + height > image_height:
+            num_annotations_skipped += 1
+            continue
+
+        if category_id == 1 and dp_triggered: # Person --> Person Face & Person Body
+            try:
+                img = dp_coco.loadImgs(image_id)[0]
+                ann_ids = dp_coco.getAnnIds(imgIds=img['id'])
+                anns = dp_coco.loadAnns(ann_ids)
+
+                for ann in anns:
+                    if 'dp_masks' in ann.keys():
+                        mask_face, mask_body = GenDPMask(ann)
+                        img_face = cv2.imread(f'{image_dir}/{filename}')
+                        img_face = cv2.cvtColor(img_face, cv2.COLOR_BGR2RGB)
+                        img_body = img_face.copy()
+
+                        bbr = np.array(ann['bbox']).astype(int)
+                        x1, y1, x2, y2 = bbr[0], bbr[1], bbr[0]+bbr[2], bbr[1]+bbr[3]
+                        x2 = min( [x2, img_face.shape[1]] )
+                        y2 = min( [y2, img_face.shape[0]] )
+
+                        # Person Face
+                        if np.count_nonzero(mask_face) != 0:
+                            category_id = -1 
+                            category_ids.append(category_id)
+                            category_names.append('Person Face'.encode('utf8'))
+
+                            mask_face = cv2.resize(mask_face, (int(x2 - x1), int(y2 - y1)), interpolation=cv2.INTER_NEAREST)
+                            mask_face = (mask_face == 1)
+                            binary_mask = np.zeros((img_face.shape[0], img_face.shape[1]))
+                            binary_mask[y1:y2, x1:x2][mask_face] = 1
+                            binary_mask = np.uint8(binary_mask)
+                            binary_mask = np.fliplr(binary_mask)   # Person Face Horizontal Flip
+                            
+                            props = regionprops(binary_mask)
+                            prop = props[0]
+
+                            xmin.append(float(prop.bbox[1]) / image_width)
+                            xmax.append(float(prop.bbox[3]) / image_width)
+                            ymin.append(float(prop.bbox[0]) / image_height)
+                            ymax.append(float(prop.bbox[2]) / image_height)
+                            is_crowd.append(ann['iscrowd'])
+                            area.append(prop.area)
+
+                            pil_image = PIL.Image.fromarray(binary_mask)
+                            output_io = io.BytesIO()
+                            pil_image.save(output_io, format='PNG')
+                            encoded_mask_png.append(output_io.getvalue())
+
+                        # Person Body
+                        if np.count_nonzero(mask_body) != 0:
+                            category_id = -2 
+                            category_ids.append(category_id)
+                            category_names.append('Person Body'.encode('utf8'))
+                            
+                            mask_body = cv2.resize(mask_body, (int(x2 - x1), int(y2 - y1)), interpolation=cv2.INTER_NEAREST)
+                            mask_body = (mask_body == 1)
+                            binary_mask = np.zeros((img_face.shape[0], img_face.shape[1]))
+                            binary_mask[y1:y2, x1:x2][mask_body] = 1
+                            binary_mask = np.uint8(binary_mask)
+                            binary_mask = np.fliplr(binary_mask)    # Person Body Horizontal Flip
+
+                            props = regionprops(binary_mask)
+                            prop = props[0]
+
+                            xmin.append(float(prop.bbox[1]) / image_width)
+                            xmax.append(float(prop.bbox[3]) / image_width)
+                            ymin.append(float(prop.bbox[0]) / image_height)
+                            ymax.append(float(prop.bbox[2]) / image_height)
+                            is_crowd.append(ann['iscrowd'])
+                            area.append(prop.area)
+
+                            pil_image = PIL.Image.fromarray(binary_mask)
+                            output_io = io.BytesIO()
+                            pil_image.save(output_io, format='PNG')
+                            encoded_mask_png.append(output_io.getvalue())
+
+                dp_triggered = False
+
+            except KeyError:
+                pass
+        elif category_id == 1 and not dp_triggered:
+            continue
+        else:
+            xmin.append(1 - float(x) / image_width)  # Xmin Horizontal Flip
+            xmax.append(1 - float(x + width) / image_width)   # Xmax Horizontal Flip
+            ymin.append(float(y) / image_height)
+            ymax.append(float(y + height) / image_height)
+            is_crowd.append(object_annotations['iscrowd'])
+
+            category_ids.append(category_id)
+            category_names.append(category_index[category_id]['name'].encode('utf8'))
+            area.append(object_annotations['area'])
+
+            run_len_encoding = mask.frPyObjects(object_annotations['segmentation'],
+                                                image_height, image_width)
+            binary_mask = mask.decode(run_len_encoding)
+
+            if not object_annotations['iscrowd']:
+                binary_mask = np.amax(binary_mask, axis=2)  # (H, W, 1) --> (H, W)
+
+            binary_mask = np.fliplr(binary_mask)  # Mask Horizontal Flip
+            pil_image = PIL.Image.fromarray(binary_mask)
+            output_io = io.BytesIO()
+            pil_image.save(output_io, format='PNG')
+            encoded_mask_png.append(output_io.getvalue())
+
+    # Label Remapping
+    # Background --> 0
+    remapping = {
+        # Person --> Person Face & Person Body
+        -1: 1,  # Person Face
+        -2: 2,  # Person Body
+        2: 3, # Bicycle
+        3: 4, # Car -> Car
+        6: 4, # Bus -> Car
+        4: 5, # Motorbike
+        5: 6, # Airplane
+        9: 7, # Ship (Boat)
+        16: 8, # Bird
+        17: 9, # Cat
+        18: 10, # Dog
+        19: 11, # Horse
+        21: 12  # Cow
+    }
+
+    category_ids = list(map(lambda x: remapping[x], category_ids))
+
+    feature_dict = {
+        'image/height':
+            dataset_util.int64_feature(r),
+        'image/width':
+            dataset_util.int64_feature(r),
+        'image/filename':
+            dataset_util.bytes_feature(filename.encode('utf8')),
+        'image/source_id':
+            dataset_util.bytes_feature(str(image_id).encode('utf8')),
+        'image/key/sha256':
+            dataset_util.bytes_feature(key.encode('utf8')),
+        'image/encoded':
+            dataset_util.bytes_feature(encoded_jpg),
+        'image/format':
+            dataset_util.bytes_feature('jpeg'.encode('utf8')),
+        'image/object/bbox/xmin':
+            dataset_util.float_list_feature(xmin),
+        'image/object/bbox/xmax':
+            dataset_util.float_list_feature(xmax),
+        'image/object/bbox/ymin':
+            dataset_util.float_list_feature(ymin),
+        'image/object/bbox/ymax':
+            dataset_util.float_list_feature(ymax),
+        'image/object/class/label_text':
+            dataset_util.bytes_list_feature(category_names),
+        'image/object/class/label_id':
+            dataset_util.int64_list_feature(category_ids),
+        'image/object/is_crowd':
+            dataset_util.int64_list_feature(is_crowd),
+        'image/object/area':
+            dataset_util.float_list_feature(area),
+        'image/object/mask':
+            dataset_util.bytes_list_feature(encoded_mask_png)
+    }
+    
+    example = tf.train.Example(features=tf.train.Features(feature=feature_dict))
+    return key, example, num_annotations_skipped, len(category_ids)
+
+
+
 def create_tf_example(image,
                       annotations_list,
                       image_dir,
@@ -126,7 +363,7 @@ def create_tf_example(image,
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = PIL.Image.open(encoded_jpg_io)
-    r = 256
+    r = 320
     image = image.resize((r, r), PIL.Image.ANTIALIAS)
     bytes_io = io.BytesIO()
     image.save(bytes_io, format='JPEG')
@@ -375,6 +612,13 @@ def _create_tf_record_from_coco_annotations(
             
             if num_crowd != len(annotations_list):
                 _, tf_example, num_annotations_skipped, num_instances = create_tf_example(image, annotations_list, image_dir, category_index)
+                if num_instances != 0:
+                    total_num_annotations_skipped += num_annotations_skipped
+                    total_num_instances += num_instances
+                    shard_idx = idx % num_shards
+                    output_tfrecords[shard_idx].write(tf_example.SerializeToString())
+
+                _, tf_example, num_annotations_skipped, num_instances = create_tf_horizontal_flip_example(image, annotations_list, image_dir, category_index)
                 if num_instances != 0:
                     total_num_annotations_skipped += num_annotations_skipped
                     total_num_instances += num_instances
