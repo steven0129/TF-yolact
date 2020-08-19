e8816081951e4c909bbd87b0d6f6692f22f7c540
steven.gao <steven.gao@realtek.com>
2020-08-19 11:29:20 +0800

    Fix mask loss with fixing ground truth bounding box width and height.
    Signed-off-by: steven.gao <steven.gao@realtek.com>

--- loss/loss_yolact.py
+++ loss/loss_yolact.py
@@ -218,7 +218,7 @@ class YOLACTLoss(object):
 
             # crop the pred (not real crop, zero out the area outside the gt box)
             s = tf.nn.sigmoid_cross_entropy_with_logits(gt, pred_mask)
-            s = utils.crop(s, bbox, origin_w=320, origin_h=320)
+            s = utils.crop(s, bbox, origin_w=80, origin_h=80)
             loss = tf.reduce_sum(s, axis=[1, 2]) / area
             loss_mask += tf.reduce_sum(loss)
 
