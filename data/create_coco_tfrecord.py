10ceb497d4d858f120e6baba878e78452ff8ef96
steven.gao <steven.gao@realtek.com>
2020-08-25 18:56:59 +0800

    Add data augmentation with motion blur and random crop.
    Signed-off-by: steven.gao <steven.gao@realtek.com>

--- data/create_coco_tfrecord.py
+++ data/create_coco_tfrecord.py
@@ -38,6 +38,9 @@ import contextlib2
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import imgaug as ia
+import imgaug.augmenters as iaa
+import random
 
 # use absl for tf 2.0
 from absl import app
@@ -84,6 +87,53 @@ def GenDPMask(ann):
         mask_face[curr_mask > 0] = 1
 
     return mask_face, mask_body
+
+def data_aug(image, bounding_boxes, binary_masks, category_ids, category_names, is_crowd, area):
+    binary_masks = ia.SegmentationMapsOnImage(binary_masks.astype(np.int32), shape=image.shape)
+    
+    bbs = []
+    for x1, x2, y1, y2 in bounding_boxes:
+        bbs.append(ia.BoundingBox(x1=x1, x2=x2, y1=y1, y2=y2))
+    bbs = ia.BoundingBoxesOnImage(bbs, shape=image.shape)
+
+    aug = [
+        iaa.Sequential([iaa.CropToFixedSize(width=320, height=320)]),
+        iaa.Sequential([iaa.MotionBlur(k=5)])
+    ]
+
+    image_aug, binary_mask_aug, bbs_aug = aug[random.randint(0, 1)](image=image, segmentation_maps=binary_masks, bounding_boxes=bbs)
+    binary_mask_aug = binary_mask_aug.get_arr()
+    binary_mask_aug[binary_mask_aug > 0] = 1
+    
+    bounding_boxes_aug = []
+    binary_masks_aug_norm = []
+    category_ids_aug = []
+    category_names_aug = []
+    is_crowd_aug = []
+    area_aug = []
+    
+    for idx, cat_id in enumerate(category_ids):
+        xmin_aug = bbs_aug[idx].x1_int if bbs_aug[idx].x1_int < image_aug.shape[1] else image_aug.shape[1]
+        xmax_aug = bbs_aug[idx].x2_int if bbs_aug[idx].x2_int < image_aug.shape[1] else image_aug.shape[1]
+        ymin_aug = bbs_aug[idx].y1_int if bbs_aug[idx].y1_int < image_aug.shape[0] else image_aug.shape[0]
+        ymax_aug = bbs_aug[idx].y2_int if bbs_aug[idx].y2_int < image_aug.shape[0] else image_aug.shape[0]
+
+        xmin_aug = xmin_aug if xmin_aug > 0 else 0
+        xmax_aug = xmax_aug if xmax_aug > 0 else 0
+        ymin_aug = ymin_aug if ymin_aug > 0 else 0
+        ymax_aug = ymax_aug if ymax_aug > 0 else 0
+
+        mask_curr = binary_mask_aug[:, :, idx]
+
+        if not (xmin_aug == xmax_aug or ymin_aug == ymax_aug or np.count_nonzero(mask_curr) == 0):
+            category_ids_aug.append(cat_id)
+            bounding_boxes_aug.append((xmin_aug, xmax_aug, ymin_aug, ymax_aug))
+            binary_masks_aug_norm.append(mask_curr)
+            category_names_aug.append(category_names[idx])
+            is_crowd_aug.append(is_crowd[idx])
+            area_aug.append(area[idx])
+
+    return image_aug, bounding_boxes_aug, binary_masks_aug_norm, category_ids_aug, category_names_aug, is_crowd_aug, area_aug
 
 def create_tf_horizontal_flip_example(image,
                       annotations_list,
@@ -321,6 +371,284 @@ def create_tf_horizontal_flip_example(image,
     return key, example, num_annotations_skipped, len(category_ids)
 
 
+def create_tf_example_imgaug(image,
+                      annotations_list,
+                      image_dir,
+                      category_index):
+    """Converts image and annotations to a tf.Example proto.
+
+    Args:
+      image: dict with keys:
+        [u'license', u'file_name', u'coco_url', u'height', u'width',
+        u'date_captured', u'flickr_url', u'id']
+      annotations_list:
+        list of dicts with keys:
+        [u'segmentation', u'area', u'iscrowd', u'image_id',
+        u'bbox', u'category_id', u'id']
+        Notice that bounding box coordinates in the official COCO dataset are
+        given as [x, y, width, height] tuples using absolute coordinates where
+        x, y represent the top-left (0-indexed) corner.  This function converts
+        to the format expected by the Tensorflow Object Detection API (which is
+        which is [ymin, xmin, ymax, xmax] with coordinates normalized relative
+        to image size).
+      image_dir: directory containing the image files.
+      category_index: a dict containing COCO category information keyed
+        by the 'id' field of each category.  See the
+        label_map_util.create_category_index function.
+    Returns:
+      example: The converted tf.Example
+      num_annotations_skipped: Number of (invalid) annotations that were ignored.
+
+    Raises:
+      ValueError: if the image pointed to by data['filename'] is not a valid JPEG
+    """
+    image_height = image['height']
+    image_width = image['width']
+    filename = image['file_name']
+    image_id = image['id']
+
+    full_path = os.path.join(image_dir, filename)
+    with tf.io.gfile.GFile(full_path, 'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = PIL.Image.open(encoded_jpg_io)
+    origin_image = image
+    r = 320
+    image = image.resize((r, r), PIL.Image.ANTIALIAS)
+    bytes_io = io.BytesIO()
+    image.save(bytes_io, format='JPEG')
+    encoded_jpg = bytes_io.getvalue()
+    key = hashlib.sha256(encoded_jpg).hexdigest()
+
+    xmin = []
+    xmax = []
+    ymin = []
+    ymax = []
+    is_crowd = []
+    category_names = []
+    category_ids = []
+    area = []
+    binary_masks = []
+    encoded_mask_png = []
+    num_annotations_skipped = 0
+    dp_triggered = True  # DensePose preprocessing just one time
+
+    for object_annotations in annotations_list:
+        category_id = int(object_annotations['category_id'])
+
+        (x, y, width, height) = tuple(object_annotations['bbox'])
+        if width <= 0 or height <= 0:
+            num_annotations_skipped += 1
+            continue
+        if x + width > image_width or y + height > image_height:
+            num_annotations_skipped += 1
+            continue
+
+        if category_id == 1 and dp_triggered: # Person --> Person Face & Person Body
+            try:
+                img = dp_coco.loadImgs(image_id)[0]
+                ann_ids = dp_coco.getAnnIds(imgIds=img['id'])
+                anns = dp_coco.loadAnns(ann_ids)
+
+                for ann in anns:
+                    if 'dp_masks' in ann.keys():
+                        mask_face, mask_body = GenDPMask(ann)
+                        img_face = cv2.imread(f'{image_dir}/{filename}')
+                        img_face = cv2.cvtColor(img_face, cv2.COLOR_BGR2RGB)
+                        img_body = img_face.copy()
+
+                        bbr = np.array(ann['bbox']).astype(int)
+                        x1, y1, x2, y2 = bbr[0], bbr[1], bbr[0]+bbr[2], bbr[1]+bbr[3]
+                        x2 = min( [x2, img_face.shape[1]] )
+                        y2 = min( [y2, img_face.shape[0]] )
+
+                        # Person Face
+                        if np.count_nonzero(mask_face) != 0:
+                            category_id = -1 
+                            category_ids.append(category_id)
+                            category_names.append('Person Face'.encode('utf8'))
+
+                            mask_face = cv2.resize(mask_face, (int(x2 - x1), int(y2 - y1)), interpolation=cv2.INTER_NEAREST)
+                            mask_face = (mask_face == 1)
+                            binary_mask = np.zeros((img_face.shape[0], img_face.shape[1]))
+                            binary_mask[y1:y2, x1:x2][mask_face] = 1
+                            binary_mask = np.uint8(binary_mask)
+                            
+                            props = regionprops(binary_mask)
+                            prop = props[0]
+
+                            xmin.append(float(prop.bbox[1]))
+                            xmax.append(float(prop.bbox[3]))
+                            ymin.append(float(prop.bbox[0]))
+                            ymax.append(float(prop.bbox[2]))
+                            is_crowd.append(ann['iscrowd'])
+                            area.append(prop.area)
+                            binary_masks.append(binary_mask)
+                            # pil_image = PIL.Image.fromarray(binary_mask)
+                            # output_io = io.BytesIO()
+                            # pil_image.save(output_io, format='PNG')
+                            # encoded_mask_png.append(output_io.getvalue())
+
+                        # Person Body
+                        if np.count_nonzero(mask_body) != 0:
+                            category_id = -2 
+                            category_ids.append(category_id)
+                            category_names.append('Person Body'.encode('utf8'))
+                            
+                            mask_body = cv2.resize(mask_body, (int(x2 - x1), int(y2 - y1)), interpolation=cv2.INTER_NEAREST)
+                            mask_body = (mask_body == 1)
+                            binary_mask = np.zeros((img_face.shape[0], img_face.shape[1]))
+                            binary_mask[y1:y2, x1:x2][mask_body] = 1
+                            binary_mask = np.uint8(binary_mask)
+
+                            props = regionprops(binary_mask)
+                            prop = props[0]
+
+                            xmin.append(float(prop.bbox[1]))
+                            xmax.append(float(prop.bbox[3]))
+                            ymin.append(float(prop.bbox[0]))
+                            ymax.append(float(prop.bbox[2]))
+                            is_crowd.append(ann['iscrowd'])
+                            area.append(prop.area)
+                            binary_masks.append(binary_mask)
+                            # pil_image = PIL.Image.fromarray(binary_mask)
+                            # output_io = io.BytesIO()
+                            # pil_image.save(output_io, format='PNG')
+                            # encoded_mask_png.append(output_io.getvalue())
+
+                dp_triggered = False
+
+            except KeyError:
+                pass
+        elif category_id == 1 and not dp_triggered:
+            continue
+        else:
+            xmin.append(float(x))
+            xmax.append(float(x + width))
+            ymin.append(float(y))
+            ymax.append(float(y + height))
+            is_crowd.append(object_annotations['iscrowd'])
+
+            category_ids.append(category_id)
+            category_names.append(category_index[category_id]['name'].encode('utf8'))
+            area.append(object_annotations['area'])
+
+            run_len_encoding = mask.frPyObjects(object_annotations['segmentation'],
+                                                image_height, image_width)
+            binary_mask = mask.decode(run_len_encoding)
+
+            if not object_annotations['iscrowd']:
+                binary_mask = np.amax(binary_mask, axis=2)  # (H, W, 1) --> (H, W)
+
+            binary_masks.append(binary_mask)
+            # pil_image = PIL.Image.fromarray(binary_mask)
+            # output_io = io.BytesIO()
+            # pil_image.save(output_io, format='PNG')
+            # encoded_mask_png.append(output_io.getvalue())
+
+    # Label Remapping
+    # Background --> 0
+    remapping = {
+        # Person --> Person Face & Person Body
+        -1: 1,  # Person Face
+        -2: 2,  # Person Body
+        2: 3, # Bicycle
+        3: 4, # Car -> Car
+        6: 4, # Bus -> Car
+        4: 5, # Motorbike
+        5: 6, # Airplane
+        9: 7, # Ship (Boat)
+        16: 8, # Bird
+        17: 9, # Cat
+        18: 10, # Dog
+        19: 11, # Horse
+        21: 12  # Cow
+    }
+
+    category_ids = list(map(lambda x: remapping[x], category_ids))
+
+    if len(category_ids) != 0:
+        bbs = []
+        for idx, _ in enumerate(category_ids):
+            bbs.append((xmin[idx], xmax[idx], ymin[idx], ymax[idx]))
+
+        binary_masks = np.stack(binary_masks, axis=-1)
+        image_aug, bbs, binary_masks, category_ids, category_names, is_crowd, area = data_aug(
+            np.array(origin_image), 
+            category_ids=category_ids, 
+            bounding_boxes=bbs, 
+            binary_masks=binary_masks,
+            category_names=category_names,
+            is_crowd=is_crowd,
+            area=area
+        )
+
+        # Process augmented bounding boxes
+        xmin = []
+        xmax = []
+        ymin = []
+        ymax = []
+
+        for x1, x2, y1, y2 in bbs:
+            xmin.append(x1)
+            xmax.append(x2)
+            ymin.append(y1)
+            ymax.append(y2)
+
+        # Process augmented masks
+        encoded_mask_png = []
+        for mask_curr in binary_masks:
+            pil_image = PIL.Image.fromarray(mask_curr)
+            output_io = io.BytesIO()
+            pil_image.save(output_io, format='PNG')
+            encoded_mask_png.append(output_io.getvalue())
+
+        # Process augmented images
+        r = 320
+        image = PIL.Image.fromarray(image_aug)
+        image = image.resize((r, r), PIL.Image.ANTIALIAS)
+        bytes_io = io.BytesIO()
+        image.save(bytes_io, format='JPEG')
+        encoded_jpg = bytes_io.getvalue()
+        key = hashlib.sha256(encoded_jpg).hexdigest()
+
+    feature_dict = {
+        'image/height':
+            dataset_util.int64_feature(r),
+        'image/width':
+            dataset_util.int64_feature(r),
+        'image/filename':
+            dataset_util.bytes_feature(filename.encode('utf8')),
+        'image/source_id':
+            dataset_util.bytes_feature(str(image_id).encode('utf8')),
+        'image/key/sha256':
+            dataset_util.bytes_feature(key.encode('utf8')),
+        'image/encoded':
+            dataset_util.bytes_feature(encoded_jpg),
+        'image/format':
+            dataset_util.bytes_feature('jpeg'.encode('utf8')),
+        'image/object/bbox/xmin':
+            dataset_util.float_list_feature(xmin),
+        'image/object/bbox/xmax':
+            dataset_util.float_list_feature(xmax),
+        'image/object/bbox/ymin':
+            dataset_util.float_list_feature(ymin),
+        'image/object/bbox/ymax':
+            dataset_util.float_list_feature(ymax),
+        'image/object/class/label_text':
+            dataset_util.bytes_list_feature(category_names),
+        'image/object/class/label_id':
+            dataset_util.int64_list_feature(category_ids),
+        'image/object/is_crowd':
+            dataset_util.int64_list_feature(is_crowd),
+        'image/object/area':
+            dataset_util.float_list_feature(area),
+        'image/object/mask':
+            dataset_util.bytes_list_feature(encoded_mask_png)
+    }
+    
+    example = tf.train.Example(features=tf.train.Features(feature=feature_dict))
+    return key, example, num_annotations_skipped, len(category_ids)
 
 def create_tf_example(image,
                       annotations_list,
@@ -489,7 +817,6 @@ def create_tf_example(image,
             if not object_annotations['iscrowd']:
                 binary_mask = np.amax(binary_mask, axis=2)  # (H, W, 1) --> (H, W)
 
-            
             pil_image = PIL.Image.fromarray(binary_mask)
             output_io = io.BytesIO()
             pil_image.save(output_io, format='PNG')
@@ -618,12 +945,22 @@ def _create_tf_record_from_coco_annotations(
                     shard_idx = idx % num_shards
                     output_tfrecords[shard_idx].write(tf_example.SerializeToString())
 
+                # Horizontal Flip Augmentation
                 _, tf_example, num_annotations_skipped, num_instances = create_tf_horizontal_flip_example(image, annotations_list, image_dir, category_index)
                 if num_instances != 0:
                     total_num_annotations_skipped += num_annotations_skipped
                     total_num_instances += num_instances
                     shard_idx = idx % num_shards
                     output_tfrecords[shard_idx].write(tf_example.SerializeToString())
+
+                # Other Data Augmentation
+                if image_dir == FLAGS.train_image_dir:
+                    _, tf_example, num_annotations_skipped, num_instances = create_tf_example_imgaug(image, annotations_list, image_dir, category_index)
+                    if num_instances != 0:
+                        total_num_annotations_skipped += num_annotations_skipped
+                        total_num_instances += num_instances
+                        shard_idx = idx % num_shards
+                        output_tfrecords[shard_idx].write(tf_example.SerializeToString())
             else:
                 total_num_annotations_skipped += len(annotations_list)
 
