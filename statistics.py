2c8a8b6bf760923fa0626aa5573e6770ab0cfc5a
steven.gao <steven.gao@realtek.com>
2020-09-28 15:09:40 +0800

    Statistics with MongoDB
    Signed-off-by: steven.gao <steven.gao@realtek.com>

---
+++ statistics.py
@@ -0,0 +1,21 @@
+import pprint
+from pymongo import MongoClient
+from collections import Counter
+from tqdm import tqdm
+
+client = MongoClient(
+    'localhost',
+    username='mongoadmin',
+    password='mongoadmin',
+    authSource='admin',
+    authMechanism='SCRAM-SHA-256'
+)
+
+collection = client['obj']['obj_256x256_20200921']
+obj_count_in_single_img = [0] * 100
+label_id_total = []
+
+for data in tqdm(collection.find({'subset': 'val'})):
+    label_id_total.extend(data['label_id'])
+
+print(obj_count_in_single_img)
