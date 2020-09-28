8280669f11038f450168bb876d1ad028a763b965
steven.gao <steven.gao@realtek.com>
2020-09-28 15:04:55 +0800

    Data loader with eval mode
    Signed-off-by: steven.gao <steven.gao@realtek.com>

--- data/dataset_coco.py
+++ data/dataset_coco.py
@@ -42,3 +42,32 @@ def prepare_dataloader(tfrecord_dir, batch_size, img_size, subset="train"):
     dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
     return dataset
+
+def prepare_evalloader(tfrecord_dir, img_size, subset="train"):
+
+    anchorobj = anchor.Anchor(img_size=img_size,
+                              feature_map_size=[32, 16, 8, 4, 2],
+                              aspect_ratio=[1, 0.5, 2],
+                              scale=[24, 48, 96, 192, 384])
+
+    parser = yolact_parser.Parser(output_size=img_size,
+                                  anchor_instance=anchorobj,
+                                  match_threshold=0.5,
+                                  unmatched_threshold=0.5,
+                                  proto_output_size=64,
+                                  mode=subset)
+    files = tf.io.matching_files(os.path.join(tfrecord_dir, "obj_%s.*" % subset))
+    num_shards = tf.cast(tf.shape(files)[0], tf.int64)
+    dataset = tf.data.Dataset.from_tensor_slices(files)
+
+    dataset = dataset.interleave(
+        tf.data.TFRecordDataset,
+        cycle_length=num_shards,
+        num_parallel_calls=tf.data.experimental.AUTOTUNE
+    )
+
+    dataset = dataset.map(map_func=parser, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    dataset = dataset.batch(1, drop_remainder=True)
+    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+    return dataset
