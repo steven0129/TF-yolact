017f1ab8c07e882ed6f970334b42fee15847b979
steven.gao <steven.gao@realtek.com>
2020-09-30 14:03:42 +0800

    CORS in flask server
    Signed-off-by: steven.gao <steven.gao@realtek.com>

--- server.py
+++ server.py
@@ -6,8 +6,10 @@ from PIL import Image
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from flask import Flask, request, send_file
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 client = MongoClient(
     'localhost',
@@ -20,11 +22,25 @@ client = MongoClient(
 collection = client['obj']['obj_256x256_20200921']
 fs = gridfs.GridFS(client['obj'])
 
+records = list(collection.find())
+
 @app.route('/img', methods=['GET'])
 def img():
-    imgID = request.args.get('id')
+    recordID = request.args.get('id')
+    imgID = records[int(recordID)]['image']
     img = io.BytesIO(fs.get(ObjectId(imgID)).read())
     return send_file(img, mimetype='image/jpg')
+
+@app.route('/data', methods=['GET'])
+def data():
+    recordID = request.args.get('id')
+    record = records[int(recordID)]
+
+    record['_id'] = str(record['_id'])
+    record['image'] = str(record['image'])
+    record['masks'] = list(map(str, record['masks']))
+
+    return record
 
 if __name__ == '__main__':
     app.debug = True
