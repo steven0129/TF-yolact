ada67ea0bb8d641683c02603981c9d81e93a8713
steven0129 <steven.gao@realtek.com>
2020-11-05 18:19:11 +0800

    Add PADModule
    Signed-off-by: steven0129 <steven.gao@realtek.com>

--- layers/protonet.py
+++ layers/protonet.py
@@ -74,3 +74,75 @@ class ProtoNet(tf.keras.layers.Layer):
         x = self.conv_final(x)
         
         return x
+
+class PADModule(tf.keras.layers.Layer):
+    def __init__(self, num_prototype):
+        super(PADModule, self).__init__()
+        
+        self.p5_conv_1x1 = tf.keras.layers.Conv2D(
+            filters=192,
+            kernel_size=(1, 1),
+            strides=1,
+            padding='same',
+            use_bias=False
+        )
+
+        self.p4_conv_3x3 = tf.keras.layers.DepthwiseConv2D(
+            kernel_size=(3, 3),
+            padding='same',
+            depth_multiplier=1,
+            strides=2,
+            use_bias=False
+        )
+
+        self.p4_conv_1x1 = tf.keras.layers.Conv2D(
+            filters=192,
+            kernel_size=(1, 1),
+            strides=1,
+            padding='same',
+            use_bias=False
+        )
+
+        self.p4_sigmoid = tf.keras.layers.Activation('sigmoid')
+
+        self.p3_conv_1x1 = tf.keras.layers.Conv2D(
+            filters=192,
+            kernel_size=(1, 1),
+            strides=1,
+            padding='same',
+            use_bias=False
+        )
+
+        self.output_conv_1x1 = tf.keras.layers.Conv2D(
+            filters=num_prototype,
+            kernel_size=(1, 1),
+            strides=1,
+            padding='same',
+            use_bias=False
+        )
+
+        self.upSampling4x4 = tf.keras.layers.UpSampling2D(size=(4, 4), interpolation='bilinear')
+        self.upSampling2x2 = tf.keras.layers.UpSampling2D(size=(2, 2), interpolation='bilinear')
+
+    def call(self, p3, p4, p5):
+        # p3 --> 1/8  * input_size
+        # p4 --> 1/16 * input_size
+        # p5 --> 1/32 * input_size
+
+        p5 = self.p5_conv_1x1(p5)   # [1/32, 1/32, 192]
+        p4 = self.p4_conv_3x3(p4)   # [1/32, 1/32, 96]
+        p4 = self.p4_conv_1x1(p4)   # [1/32, 1/32, 192]
+        p4 = self.p4_sigmoid(p4)    # [1/32, 1/32, 192]
+        p3 = self.p3_conv_1x1(p3)   # [1/8, 1/8, 192]
+
+        feature_map = p5 * p4
+        feature_map = p5 + feature_map
+
+        feature_map = self.upSampling4x4(feature_map)   # [1/8, 1/8, 192]
+        feature_map = p3 + feature_map                  # [1/8, 1/8, 192]
+        feature_map = self.upSampling2x2(feature_map)   # [1/4, 1/4, 192]
+        prototypes = self.output_conv_1x1(feature_map)  # [1/4, 1/4, 96]
+
+        tf.print(tf.shape(prototypes))
+
+        return prototypes
