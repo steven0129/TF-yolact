849774c8a3020e078cc41f9a679d7391aea7c120
steven.gao <steven.gao@realtek.com>
2020-11-11 19:38:06 +0800

    Statistics for dataset 20201102
    Signed-off-by: steven.gao <steven.gao@realtek.com>

--- statistics.py
+++ statistics.py
@@ -1,4 +1,5 @@
 import pprint
+import csv
 from pymongo import MongoClient
 from collections import Counter
 from tqdm import tqdm
@@ -11,11 +12,48 @@ client = MongoClient(
     authMechanism='SCRAM-SHA-256'
 )
 
-collection = client['obj']['obj_256x256_20200930']
+remapping = [
+    'Background',
+    'Face',
+    'Body',
+    'Bicycle',
+    'Car',
+    'Motorbike',
+    'Airplane',
+    'Ship',
+    'Bird',
+    'Cat',
+    'Dog',
+    'Horse',
+    'Cow'
+]
+
+collection = client['obj']['obj_256x256_20201102']
 obj_count_in_single_img = [0] * 100
-label_id_total = []
+label_id_test = []
+label_id_train = []
 
 for data in tqdm(collection.find({'subset': 'val'})):
-    label_id_total.extend(data['label_id'])
+    label_id_test.extend(data['label_id'])
 
-print(obj_count_in_single_img)
+for data in tqdm(collection.find({'subset': 'train'})):
+    label_id_train.extend(data['label_id'])
+
+counter_label_id_test = dict(Counter(label_id_test))
+counter_label_id_train = dict(Counter(label_id_train))
+
+with open('statistics-info/obj-count.csv', 'w') as FILE:
+    writer = csv.writer(FILE)
+    writer.writerow(['Dataset'] + remapping[1:13])
+    
+    row = ['Testing Set']
+    for i in range(1, 13):
+        row.append(counter_label_id_test[i])
+
+    writer.writerow(row)
+
+    row = ['Training Set']
+    for i in range(1, 13):
+        row.append(counter_label_id_train[i])
+
+    writer.writerow(row)
